4ed8c25b5bccb2c0f8c3d34da09d87fdbca94501
danimatuko <danimatuko@outlook.com>
2025-10-31 23:18:59 +0200

    build the image and run tests

--- jenkins/pipeline-utils.groovy
+++ jenkins/pipeline-utils.groovy
@@ -1,9 +1,11 @@
 def buildApp() {
     echo '🏗️ Building the application...'
+    sh "docker build -t node-app:latest ."
 }
 
 def runTest() {
     echo '🧪 Running tests...'
+    sh "npm run test"
 }
 
 def deploy() {
