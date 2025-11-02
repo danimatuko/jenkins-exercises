db957432ba9d4dbc91428bd0bfa1d9ea0824b4de
danimatuko <danimatuko@outlook.com>
2025-11-02 11:54:29 +0200

    added automatic deploy

--- jenkins/pipeline-utils.groovy
+++ jenkins/pipeline-utils.groovy
@@ -15,20 +15,24 @@ def runTest() {
 def incrementVersion(){
     dir('app') {
         sh 'npm version patch --no-git-tag-version'
-
         def packageJson = readJSON file: 'package.json'
         env.APP_VERSION = packageJson.version
     }
 } 
 
 def buildApp() {
-    echo '🏗️ Building the application...'
-    echo "Building version: ${env.APP_VERSION}"
-    sh "docker build -t node-app:${env.APP_VERSION} ."
+    echo "🏗️ Building version: ${env.APP_VERSION}"
+    sh "docker build -t node-app:${env.APP_VERSION} -t node-app:latest ."
 }
 
 def deploy() {
-    echo "🚀 Deploying branch: ${env.BRANCH_NAME}"
+    echo "🚀 Deploying app..."
+    // Stop/remove old container, then run new one
+    sh '''
+    docker stop my-node-app || true
+    docker rm my-node-app || true
+    docker run -d -p 3000:3000 --name my-node-app node-app:latest
+    '''
 }
 
 return this
